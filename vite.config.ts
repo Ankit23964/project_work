8384fe2de348692aa5541a7dd1cd14f89a270491
Ankit23964 <ankushetty7@gmail.com>
2025-04-23 17:48:27 +0530

    jg

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/project_work",
+  base: "/project_work/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
